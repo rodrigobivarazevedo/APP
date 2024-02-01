5a0190c38379114b666b9c5b553cc9fe3f004cca
Rodrigo Azevedo <rodrigoazevedo@Rodrigos-MacBook-Pro.local>
2024-02-01 11:13:16 +0100

    modified for books

--- app/app.py
+++ app/app.py
@@ -8,51 +8,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirn
 db = SQLAlchemy(app)
 fake = Faker()
 
-app.secret_key = 'secret_key'
-
-class Users(db.Model):
-    UserID = db.Column(db.Integer, primary_key=True)
-    First_name = db.Column(db.String(50), nullable=False)
-    Last_name = db.Column(db.String(50), nullable=False)
-    Email = db.Column(db.String(120), unique=True, nullable=False)
-    Role = db.Column(db.String(50), nullable=False, default='user')  
-
+class Books(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.String(100), nullable=False)
+    Author = db.Column(db.String(100), nullable=False)
+    Pub_year = db.Column(db.Integer, nullable=False)
 
 @app.route('/')
-def index():
-    users = Users.query.all()
-    if not users:
-        users = "No users found"
-    return render_template('index.html', users=users)
+def books():
+    book_list = Books.query.all()
+    return render_template('books.html', books=book_list)
 
-@app.route('/add_user', methods=['GET', 'POST'])
-def add_user():
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
     if request.method == 'POST':
-        Firstname = request.form['firstname']
-        Lastname = request.form['lastname']
-        Email = request.form['email']
-        
+        title = request.form['title']
+        author = request.form['author']
+        pub_year = request.form['pub_year']
 
-        new_user = Users(First_name=Firstname, Last_name=Lastname, Email=Email)
-        db.session.add(new_user)
+        new_book = Books(Title=title, Author=author, Pub_year=pub_year)
+        db.session.add(new_book)
         db.session.commit()
-        flash(f"Added {Firstname} {Lastname} successfully!", 'success')
-        return redirect(url_for('index'))
 
-    return render_template('add_user.html')
+        return redirect(url_for('books'))
 
-@app.route('/generate_fake_users')
-def generate_fake_users():
-    for _ in range(2):
-        fake_firstname = fake.first_name()
-        fake_lastname = fake.last_name()
-        fake_email = fake.email()
-        
+    return render_template('add_book.html')
 
-        new_fake_user = Users(First_name=fake_firstname, Last_name=fake_lastname, Email=fake_email)
-        db.session.add(new_fake_user)
-        db.session.commit()
-    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/db/create_tables.py
+++ app/db/create_tables.py
@@ -1,25 +1,24 @@
 import sqlite3
 
-def create_users_table(db_file):
+def create_books_table(db_file):
     try:
         # Connect to the SQLite database
         conn = sqlite3.connect(db_file)
         cursor = conn.cursor()
 
-        # Create the users table
+        # Create the books table
         cursor.execute('''
-            CREATE TABLE Users (
-                UserID INTEGER PRIMARY KEY AUTOINCREMENT,
-                First_name VARCHAR(255) NOT NULL,
-                Last_name VARCHAR(255) NOT NULL,
-                Email VARCHAR(255) NOT NULL UNIQUE,
-                Role VARCHAR(50) NOT NULL DEFAULT 'user' -- Default role is set to 'user'
+            CREATE TABLE Books (
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                Title VARCHAR(255) NOT NULL,
+                Author VARCHAR(255) NOT NULL,
+                Pub_year INTEGER NOT NULL
             );
         ''')
-
+        
         # Commit the changes (creating the table)
         conn.commit()
-        print("Table 'users' created successfully.")
+        print("Table 'books' created successfully.")
 
     except sqlite3.Error as e:
         print("SQLite error:", e)
@@ -32,5 +31,5 @@ def create_users_table(db_file):
 # Specify the name of your SQLite database file
 db_file = "database.db"
 
-# Call the function to create the 'Users' table
-create_users_table(db_file)
+# Call the function to create the 'Books' table
+create_books_table(db_file)
